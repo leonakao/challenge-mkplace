70f7d85fe2b971da61f892d51942d8fc6700179b
leonakao <leonakao.dev@gmail.com>
2022-09-21 12:46:01 -0300

    feat: implement mongo database

---
+++ src/infra/database/mongo-database-adapter.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import Database from 'src/main/database';
+import mongoose from 'mongoose';
+
+export default class MongoDatabaseAdapter implements Database {
+  private readonly uri: string;
+
+  constructor(uri: string) {
+    this.uri = uri;
+
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connection is open');
+    });
+
+    mongoose.connection.on('disconnect', this.connect);
+  }
+
+  async connect(): Promise<void> {
+    await mongoose.connect(this.uri, {
+      autoIndex: true,
+    }, (error) => {
+      console.log('error to connect to database', error);
+    });
+
+    console.log('Connected to database');
+  }
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,14 @@
 import { config as dotenvConfig } from 'dotenv';
+import MongoDatabaseAdapter from 'src/infra/database/mongo-database-adapter';
 import App from './app';
 import routesResolver from './routes';
 
-export default function start(app: App) {
+export default function start(app: App): void {
   dotenvConfig();
+
+  app.setDatabase(
+    new MongoDatabaseAdapter(process.env.MONGO_URI as string),
+  );
 
   app.setRoutes(routesResolver);
 
