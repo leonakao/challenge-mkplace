c636ef7c1ea173679e929ab916594eb8f7411151
leonakao <leonakao.dev@gmail.com>
2022-09-21 14:04:18 -0300

    feat: add AddProductUseCase in container

--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -4,6 +4,8 @@ import { container } from 'tsyringe';
 import AddProductRepository from 'src/modules/products/domain/repositories/add-product-repository';
 import ProductRepositoryImplementation from 'src/modules/products/data/repositories/product-repository.implementation';
 import productModel, { ProductDocument } from 'src/modules/products/data/models/product-model';
+import AddProductUseCaseImplementation from 'src/modules/products/domain/use-cases/implementations/add-product-use-case.implementation';
+import AddProductUseCase from 'src/modules/products/domain/use-cases/add-product-use-case';
 
 container.register<AddProductRepository>(
   'AddProductRepository',
@@ -13,4 +15,9 @@ container.register<AddProductRepository>(
 container.register(
   Model<ProductDocument>,
   productModel,
+)
+
+container.register<AddProductUseCase>(
+  'AddProductUseCase',
+  AddProductUseCaseImplementation,
 )
