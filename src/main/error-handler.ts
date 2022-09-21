49b6fd5e6fc383bae5bca72c3f2540d3726cc5f9
leonakao <leonakao.dev@gmail.com>
2022-09-21 16:24:45 -0300

    feat: implement a error handler default

--- src/main/error-handler.ts
+++ src/main/error-handler.ts
@@ -1,18 +1,38 @@
-/* eslint-disable no-console */
 import { Response } from 'src/modules/shared/presentation';
+import { ValidationError } from 'yup';
 
 export interface IErrorHandler {
   handle(error: Error): Response;
 }
 
+export interface ICommonError{
+  statusCode: number;
+  message: string;
+}
+
 export default class ErrorHandler implements IErrorHandler {
-  handle(error: Error): Response {
-    console.error(error);
+  handle(error: Error|ValidationError|ICommonError): Response {
+    if (error instanceof ValidationError) {
+      error as ValidationError;
+      return {
+        statusCode: 422,
+        body: {
+          message: error.message,
+          errors: error.inner.reduce((acc, curr) => {
+            acc[curr.path] = curr.errors
+
+            return acc;
+          }, {}),
+        },
+      }
+    }
+
+    const statusCode = (error as ICommonError).statusCode || 500;
 
     return {
-      statusCode: 500,
+      statusCode,
       body: {
-        message: 'Internal server error',
+        message: error.message || 'Internal server error',
       },
     }
   }
