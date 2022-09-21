2b563be93fac65bc416f8bafc31c395d499e300b
leonakao <leonakao.dev@gmail.com>
2022-09-21 11:32:46 -0300

    feat: create a express router adapter

---
+++ src/infra/express/express-router-adapter.ts
@@ -0,0 +1,48 @@
+import { Request as ExpressRequest, Response as ExpressResponse, Router as ExpressRouter } from 'express';
+import Controller from 'src/modules/shared/presentation/controller';
+import Router from '../../main/router'
+import ExpressRequestAdapter from './express-request-adapter';
+
+export default class ExpressRouterAdapter implements Router {
+  private readonly expressRouter: ExpressRouter
+
+  constructor(expressRouter: ExpressRouter) {
+    this.expressRouter = expressRouter
+  }
+
+  get(path: string, controller: Controller): void {
+    this.expressRouter.get(path, async (request: ExpressRequest, response: ExpressResponse) => {
+      const httpResponse = await controller.handle(new ExpressRequestAdapter(request))
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    })
+  }
+
+  post(path: string, controller: Controller): void {
+    this.expressRouter.post(path, async (request: ExpressRequest, response: ExpressResponse) => {
+      const httpResponse = await controller.handle(new ExpressRequestAdapter(request))
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    })
+  }
+
+  put(path: string, controller: Controller): void {
+    this.expressRouter.put(path, async (request: ExpressRequest, response: ExpressResponse) => {
+      const httpResponse = await controller.handle(new ExpressRequestAdapter(request))
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    })
+  }
+
+  delete(path: string, controller: Controller): void {
+    this.expressRouter.delete(path, async (request: ExpressRequest, response: ExpressResponse) => {
+      const httpResponse = await controller.handle(new ExpressRequestAdapter(request))
+      response.status(httpResponse.statusCode).json(httpResponse.body)
+    })
+  }
+
+  extends(path: string, router: Router): void {
+    this.expressRouter.use(path, (router as ExpressRouterAdapter).expressRouter)
+  }
+
+  getExpressRouter(): ExpressRouter {
+    return this.expressRouter
+  }
+}
