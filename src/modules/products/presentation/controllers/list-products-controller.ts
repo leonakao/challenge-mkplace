6a3603aae1e50a66015116701cf40bb15c727e1b
leonakao <leonakao.dev@gmail.com>
2022-09-21 17:22:33 -0300

    feat: create controller to list products

---
+++ src/modules/products/presentation/controllers/list-products-controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Request, Response } from 'src/modules/shared/presentation';
+import { inject, injectable } from 'tsyringe';
+import ListProductsUseCase, { ListOptions } from '../../domain/use-cases/list-products-use-case';
+
+@injectable()
+export default class ListProductsController implements Controller {
+  constructor(
+    @inject('ListProductsUseCase')
+    private readonly listProductsUseCase: ListProductsUseCase,
+  ) {}
+
+  async handle(request: Request): Promise<Response> {
+    const query = request.query();
+
+    const listOptions: ListOptions = {
+      perPage: query.perPage ? Number(query.perPage) : 10,
+      page: query.page ? Number(query.page) : 1,
+    }
+
+    const products = await this.listProductsUseCase.execute(
+      listOptions,
+    );
+
+    return {
+      statusCode: 201,
+      body: {
+        data: products,
+      },
+    }
+  }
+}
