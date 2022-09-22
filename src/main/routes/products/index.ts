12bfaec23d14f192f2c733796f8943ff5cb8e13e
leonakao <leonakao.dev@gmail.com>
2022-09-22 12:42:37 -0300

    feat: add new route

--- src/main/routes/products/index.ts
+++ src/main/routes/products/index.ts
@@ -1,11 +1,13 @@
-import AddProductController from 'src/modules/products/presentation/controllers/add-product-controller';
-import ListProductsController from 'src/modules/products/presentation/controllers/list-products-controller';
 import { container } from 'tsyringe';
+import AddProductController from '../../../modules/products/presentation/controllers/add-product-controller';
+import ListProductsController from '../../../modules/products/presentation/controllers/list-products-controller';
+import ShowProductController from '../../../modules/products/presentation/controllers/show-product-controller';
 import Router from '../../router';
 
 export default (router: Router) => {
   router.post('/', container.resolve(AddProductController));
   router.get('/', container.resolve(ListProductsController));
+  router.get('/:slug', container.resolve(ShowProductController));
 
   return router;
 }
