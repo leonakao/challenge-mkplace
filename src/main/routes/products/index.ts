09b40cccb4f5e042304ab66197a312703b9af85e
leonakao <leonakao.dev@gmail.com>
2022-09-22 11:14:27 -0300

    feat: add route to list products

--- src/main/routes/products/index.ts
+++ src/main/routes/products/index.ts
@@ -1,9 +1,11 @@
 import AddProductController from 'src/modules/products/presentation/controllers/add-product-controller';
+import ListProductsController from 'src/modules/products/presentation/controllers/list-products-controller';
 import { container } from 'tsyringe';
 import Router from '../../router';
 
 export default (router: Router) => {
   router.post('/', container.resolve(AddProductController));
+  router.get('/', container.resolve(ListProductsController));
 
   return router;
 }
