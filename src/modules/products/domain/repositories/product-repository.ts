17e1616eaf8a1ca671d1fe2e0bc83ef81afbe0f3
leonakao <leonakao.dev@gmail.com>
2022-09-22 11:34:36 -0300

    feat: repository should return a paginated data

--- src/modules/products/domain/repositories/product-repository.ts
+++ src/modules/products/domain/repositories/product-repository.ts
@@ -1,8 +1,9 @@
+import PaginatedDataStruct from 'src/modules/shared/data/data-strcucts/paginated-data-struct';
 import Product from '../../entity/product';
 import { AddProductData } from '../use-cases/add-product-use-case';
 
 export default interface ProductRepository {
   addProduct(product: AddProductData): Promise<Product>;
   findByName(name: string): Promise<Product | null>;
-  listProducts(perPage: number, page: number): Promise<Product[]>;
+  listProducts(perPage: number, page: number): Promise<PaginatedDataStruct<Product>>;
 }
